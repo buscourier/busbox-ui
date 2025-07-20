627695250bbf1523193979e0a7e02ac0d0fd90c8
Mayatskiy Anton <work.mayatskiy@gmail.com>
2025-07-20 17:49:55 +1000

    feat(news): add NewsCardComponent to display individual news items
    - Show image, title, and teaser for a news article
    - Add "Read more" link with navigation to full news page
    - Integrate Taiga UI Skeleton for image loading state
    - Apply hover animation and shadow for interactive effect
    - Handle image load and error states gracefully
    - Create responsive layout

---
+++ src/app/features/news/news-card/news-card.component.ts
@@ -0,0 +1,39 @@
+import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TuiIcon } from '@taiga-ui/core';
+import { TuiSkeleton } from '@taiga-ui/kit';
+
+import { cn } from '@core/utils';
+
+import type { NewsItem } from '../types';
+
+@Component({
+  selector: 'app-news-card',
+  imports: [TuiIcon, RouterLink, TuiSkeleton],
+  templateUrl: './news-card.component.html',
+  styleUrl: './news-card.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NewsCardComponent {
+  @Input() article!: NewsItem;
+
+  @HostBinding('class') get hostClasses(): string {
+    return cn(
+      'block bg-white rounded-xl overflow-hidden transition-all duration-300',
+      'shadow-lg  hover:-translate-y-1 hover:shadow-xl',
+      'border-8 border-white',
+    );
+  }
+
+  imageLoaded = false;
+  imageError = false;
+
+  onImageLoad(): void {
+    this.imageLoaded = true;
+  }
+
+  onImageError(): void {
+    this.imageError = true;
+    this.imageLoaded = true; // Скрываем лоадер даже при ошибке
+  }
+}
