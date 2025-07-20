431ac30106647b27f76a380da739698ce6207cb3
Mayatskiy Anton <work.mayatskiy@gmail.com>
2025-07-20 17:20:03 +1000

    feat(home): create StatsSummaryComponent
    - Add StatCardComponent for individual statistic items with circular badges
    - Add support for both dynamic city counts and static numeric values
    - Implement responsive layout

---
+++ src/app/features/home/components/stats-summary/stat-card/stat-card.component.ts
@@ -0,0 +1,40 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  HostBinding,
+  Input,
+  Output,
+} from '@angular/core';
+import { TuiIcon } from '@taiga-ui/core';
+
+import { cn } from '@core/utils';
+
+import type { DeliveryCity, PickupCity } from '@shared/types';
+
+@Component({
+  selector: 'app-stat-card',
+  imports: [TuiIcon],
+  templateUrl: './stat-card.component.html',
+  styleUrl: './stat-card.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class StatCardComponent {
+  @Input() cities!: PickupCity[] | DeliveryCity[];
+  @Input({ required: true }) title!: string;
+  @Input() text!: string;
+  @Input() count = 0;
+  @Output() showCities = new EventEmitter<PickupCity[] | DeliveryCity[]>();
+
+  @HostBinding('class') get hostClasses(): string {
+    return cn(
+      'mb-7 flex items-center gap-x-5',
+      'md:flex-col md:items-start md:gap-x-0',
+      'lg:mb-0 xl:mb-12 xl:flex-row xl:items-center xl:gap-x-8',
+    );
+  }
+
+  onShowCities() {
+    this.showCities.emit(this.cities);
+  }
+}
