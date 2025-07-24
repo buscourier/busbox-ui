ce45871d9a7709b1c757a916455b2e8ce2ba8ef6
Mayatskiy Anton <work.mayatskiy@gmail.com>
2025-07-24 16:20:21 +1000

    refactor(shared): update DocumentCardComponent styling and icon handling

--- src/app/shared/components/document-card/document-card.component.ts
+++ src/app/shared/components/document-card/document-card.component.ts
@@ -1,11 +1,21 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  HostBinding,
+  Input,
+  Output,
+} from '@angular/core';
 import { TuiIcon } from '@taiga-ui/core';
+
+import { cn } from '@core/utils';
 
 import type { DocumentFile } from '@shared/types';
 
 @Component({
   selector: 'app-document-card',
-  imports: [TuiIcon],
+  imports: [TuiIcon, NgOptimizedImage],
   templateUrl: './document-card.component.html',
   styleUrl: './document-card.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -13,6 +23,12 @@ import type { DocumentFile } from '@shared/types';
 export class DocumentCardComponent {
   @Input({ required: true }) doc!: DocumentFile;
   @Output() show = new EventEmitter<DocumentFile>();
+
+  @HostBinding('class') get hostClasses(): string {
+    return cn('flex max-w-[370px] items-start rounded-sm pt-6 pr-9 pb-8 pl-7 shadow-xl');
+  }
+
+  protected readonly cn = cn;
 
   onShow() {
     this.show.emit(this.doc);
