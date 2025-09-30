059031b79b6596ef54ce21e5d4f717378e019d01
Mayatskiy Anton <work.mayatskiy@gmail.com>
2025-10-01 03:53:29 +1000

    refactor(account/layout): improve logout functionality and styling

--- src/app/features/account/layout/layout.component.ts
+++ src/app/features/account/layout/layout.component.ts
@@ -7,6 +7,7 @@ import { NavigationService } from '@core/services/navigation.service';
 import { PageLayoutComponent } from '@shared/layouts/page-layout';
 
 import { BalanceComponent } from '@account/balance';
+import { AuthFacade } from '@auth';
 
 @Component({
   selector: 'app-layout',
@@ -16,7 +17,13 @@ import { BalanceComponent } from '@account/balance';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LayoutComponent {
-  private readonly navigationService = inject(NavigationService);
+  private readonly navigation = inject(NavigationService);
+  private readonly auth = inject(AuthFacade);
 
-  accountLinks = this.navigationService.getDropdownItems('account');
+  accountLinks = this.navigation.getDropdownItems('account');
+  bookingLink = '/' + this.navigation.findByLink('booking')!.link;
+
+  logout() {
+    this.auth.logout();
+  }
 }
